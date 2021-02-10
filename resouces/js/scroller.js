ed1324eb2c57b103a7b9e00d8cb13b8954df8beb
hoyamono <yourde@naver.com>
2021-02-10 12:25:37 +0900

    fix :trackElement 높이가 0일 경우 기본 높이값 설정 및 box-sizing 스타일 추가

--- resouces/js/scroller.js
+++ resouces/js/scroller.js
@@ -134,9 +134,12 @@ var SCROLLER = function () {
       }
     },
     setTrackHeigh: function () {
+      this.trackElement.style.height = '';
       this.trackElement.style.paddingTop = '', this.trackElement.style.paddingBottom = '';
       var isTrackHeight = this.trackElement.clientHeight == 0 ? this.windowHeight : this.trackElement.clientHeight,
           calTrackHeight = isTrackHeight * this.trackHeight - isTrackHeight;
+      this.trackElement.style.boxSizing = 'content-box';
+      this.trackElement.style.height = this.windowHeight + 'px';
       this.trackElement.style.paddingTop = calTrackHeight / 2 + 'px';
       this.trackElement.style.paddingBottom = calTrackHeight / 2 + 'px';
     },
